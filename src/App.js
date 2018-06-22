4eb47a52e95ed09f7916b38039a2f034a02596b2
Mukesh Srivastava <mukeshsrivastav691@gmail.com>
2018-06-23 01:38:53 +0530

    List and map out other details of the objects

--- src/App.js
+++ src/App.js
@@ -27,7 +27,16 @@ class App extends Component {
     return (
       <div className="App">
         {list.map(function(item) {
-          return <div>{item.title}</div>;
+          return (
+            <div>
+              <span>
+                <a href={item.url}>{item.title}</a>
+              </span>
+              <span>{item.author}</span>
+              <span>{item.num_comments}</span>
+              <span>{item.points}</span>
+            </div>
+          );
         })}
       </div>
     );
