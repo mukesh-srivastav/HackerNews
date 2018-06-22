e6cfbaaf20d217a02686952570bded978fb525df
Mukesh Srivastava <mukeshsrivastav691@gmail.com>
2018-06-23 01:28:26 +0530

    Added dummy api response into index.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,26 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const list = [
+        {
+        title: 'React',
+        url: 'https://facebook.github.io/react/',
+        author: 'Jordan Walke',
+        num_comments: 3,
+        points: 4,
+        objectID: 0,
+        },
+        {
+        title: 'Redux',
+        url: 'https://github.com/reactjs/redux',
+        author: 'Dan Abramov, Andrew Clark',
+        num_comments: 2,
+        points: 5,
+        objectID: 1,
+        },
+    ];
+
+
 class App extends Component {
   render() {
     const username = {
