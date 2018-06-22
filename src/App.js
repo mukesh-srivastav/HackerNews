5e51f035cae68659cdaf7091277e08936eda2fe0
Mukesh Srivastava <mukeshsrivastav691@gmail.com>
2018-06-23 01:58:39 +0530

    Remove return statement as per ES6 standard of implicit return statement for arrow function

--- src/App.js
+++ src/App.js
@@ -26,8 +26,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        {list.map(item => {
-          return (
+        {list.map(item =>
             <div key={item.objectID}>
               <span>
                 <a href={item.url}>{item.title}</a>
@@ -36,8 +35,8 @@ class App extends Component {
               <span>{item.num_comments}</span>
               <span>{item.points}</span>
             </div>
-          );
-        })}
+          
+        )}
       </div>
     );
   }
